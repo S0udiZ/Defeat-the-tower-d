db349d5a6f1b156fec3ad85115ba939ee54e1b4c
toke0904 <tokealsbjerg@gmail.com>
2024-11-12 10:00:24 +0100

    test

---
+++ Fairtael/Assets/scripts/playercontroller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed = 5;
+    public Rigidbody2D rb;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mover
+        Vector3 direction = new Vector3(0, 0, 0);
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        direction = Vector3.ClampMagnitude(direction, 1);
+        direction *= Speed;
+        rb.velocity = direction;
+    }
+}
