ad80ed8760297c2ce8ea8588eea861b37fd2a8c5
Darwin-our-lord <David@prebensen.dk>
2024-11-12 15:35:14 +0100

    More Speed
    changed base speed dipshit

--- Fairtael/Assets/scripts/player/playercontroller.cs
+++ Fairtael/Assets/scripts/player/playercontroller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float Speed = 5;
+    public float Speed = 10;
     public Rigidbody2D rb;
 
 
